22226499a7b1c78ed36fe7a9f27d58cb73d1c66e
Kent Shefchek <kent.shefchek@helix.com>
2023-12-11 21:14:22 -0500

    refactor to not use var

--- src/main/scala/DayOne/Calibrator.scala
+++ src/main/scala/DayOne/Calibrator.scala
@@ -22,18 +22,22 @@ def sumCalibrationValues(source: BufferedSource): Int =
 /** Day 1 part 2
   */
 def convertWordToIntChar(encryptedCode: String): String =
-  var decryptedCode = ""
   val numbers = List("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
   val myMap: HashMap[String, String] =
     numbers.zipWithIndex.map { case (number, index) => number -> (index + 1).toString }.to(HashMap)
 
   val numberRegex = ("(?=(" + numbers.mkString("|") + "|\\d))").r
 
-  for patternMatch <- numberRegex.findAllIn(encryptedCode).matchData do
-    decryptedCode = decryptedCode + myMap.getOrElse(patternMatch.group(0), patternMatch.group(0))
-      + myMap.getOrElse(patternMatch.group(1), patternMatch.group(1))
-
-  decryptedCode
+  numberRegex
+    .findAllIn(encryptedCode)
+    .matchData
+    .flatMap({ patternMatch =>
+      List(
+        myMap.getOrElse(patternMatch.group(0), patternMatch.group(0)),
+        myMap.getOrElse(patternMatch.group(1), patternMatch.group(1))
+      )
+    })
+    .mkString("")
 
 def decryptCalibration(source: BufferedSource): Int =
   source.getLines.to(LazyList).map(convertWordToIntChar).map(getCalibrationValue).sum
